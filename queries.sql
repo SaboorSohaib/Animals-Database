205dad154a3cbbababd465e00f492901723670dc
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-20 20:49:10 +0430

    Add queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,32 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name<>'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3 ;
+
+/* Update Table */
+BEGIN; 
+UPDATE animals SET species='unspecified';
+rollback;
+
+BEGIN;
+UPDATE animals SET species='digimon' WHERE NAME '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT one;
+UPDATE animals SET weight_kg = weight_kg * (-1);
+ROLLBACK TO SAVEPOINT one;
+UPDATE animals SET weight_kg = weight_kg * (-1) WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(name) AS total_animlas FROM animals;
+SELECT name, SUM(escape_attempts) FROM animals GROUP BY name having MIN(escape_attempts) < 1;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
