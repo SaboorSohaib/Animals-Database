1168aab8b75ab00c5da46ac977d8bc411b667cc9
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-21 08:46:04 +0430

    Add some changes

--- queries.sql
+++ queries.sql
@@ -37,3 +37,5 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
+
+/* Multiple Table */
