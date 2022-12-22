496afaf1b52dd64b1573502d33c7e0365740bcd1
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-22 23:26:09 +0430

    Add query

--- queries.sql
+++ queries.sql
@@ -47,3 +47,6 @@ SELECT species.name, count(*) FROM species JOIN animals ON species.id = animals.
 SELECT name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Jennifer Orwell' AND animals.name LIKE '%mon';
 SELECT name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
 SELECT owners.full_name as full_name , COUNT(*) as count FROM animals JOIN owners ON animals.owner_id = owners.id GROUP BY owners.full_name ORDER BY count DESC LIMIT 1;
+
+/* Join Table */
+SELECT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name='William Tatcher' ORDER BY visits.visit_date DESC LIMIT 1;
