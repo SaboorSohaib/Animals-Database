45d923516fc0f34ee04c6eff71773eb6d53b698f
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-22 13:05:01 +0430

    Add queries

--- queries.sql
+++ queries.sql
@@ -42,3 +42,8 @@ SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1
 
 SELECT name FROM animals INNER JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
 SELECT * FROM animals INNER JOIN species ON  animals.species_id = species.id WHERE species.id = 1;
+SELECT full_name,name FROM animals RIGHT JOIN owners ON animals.owner_id = owners.id;
+SELECT species.name, count(*) FROM species JOIN animals ON species.id = animals.species_id GROUP BY species.name;
+SELECT name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Jennifer Orwell' AND animals.name LIKE '%mon';
+SELECT name FROM animals JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
+SELECT owners.full_name as full_name , COUNT(*) as count FROM animals JOIN owners ON animals.owner_id = owners.id GROUP BY owners.full_name ORDER BY count DESC LIMIT 1;
