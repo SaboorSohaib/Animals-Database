e53f2431bd1ca5e8fc78b661f86743b21f367841
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-24 10:31:13 +0430

    Add two queries

--- queries.sql
+++ queries.sql
@@ -51,3 +51,5 @@ SELECT owners.full_name as full_name , COUNT(*) as count FROM animals JOIN owner
 /* Join Table */
 
 SELECT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name='William Tatcher' ORDER BY visits.visit_date DESC LIMIT 1;
+SELECT COUNT(*) AS Different_Animals FROM (SELECT DISTINCT animals.name FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON visits.vets_id = vets.id WHERE vets.name='Stephanie Mendez') AS foo;
+SELECT vets.name, species.name FROM vets FULL JOIN specialization ON vets.id = specialization.vets_id FULL JOIN species ON specialization.species_id = species.id;
