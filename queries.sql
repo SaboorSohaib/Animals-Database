e82010ea83abb3bea7b078d50b2389ff706ca649
Abdul Saboor Sohaib <abdulsaboorsohaib@gmail.com>
2022-12-25 23:32:11 +0430

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+SELECT * FROM animals;
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals WHERE date_of_birth > '2016-01-01' AND date_of_birth < '2019-01-01';
 SELECT name FROM animals WHERE neutered=true AND escape_attempts < 3;
